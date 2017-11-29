d7c9bc7830bc5770efc57557bd3f64a501f93f1a
Anna Vigolajnen <vigolajnen@gmail.com>
2017-11-29 12:44:29 +0300

    правки по заданию

--- js/pictures.js
+++ js/pictures.js
@@ -1,57 +1,84 @@
 'use strict';
 
-//функция генерации случайных данных
-//функция создания DOM-элемента на основе JS-объекта,
-//функция заполнения блока DOM-элементами на основе массива JS-объектов
+var COMMENTS_PHRASES = ['Всё отлично!',
+'В целом всё неплохо. Но не всё.',
+'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-//функция генерации случайных данных
-function randomDataSet(url, like, comment) {
-  this.url = url;
-  this.like = like;
-  this.comment = comment;
-}
+var commentsPhotosRandIndex = Math.floor(Math.random() * (COMMENTS_PHRASES.length));
 
 function randomInt(min, max) {
   var rand = min - 0.5 + Math.random() * (max - min + 1);
   rand = Math.round(rand);
   return rand;
 };
-var photoAddresses = [];
-for(var i = 1; i <= 25; i++) {
-  var photoAddressesItem = '../kekstagram/photos/'+i+'.jpg';
-  photoAddresses.push(photoAddressesItem);
-}
-var numberLikes = [];
-for (i = 15;i <= 200;i++) {
-    numberLikes[i] = i;
+var randomNumber = Math.random();
+
+var gallery = document.querySelector('.gallery-overlay');
+gallery.classList.remove('hidden');
+var galleryPreviewElement = document.querySelector('.gallery-overlay-preview').content;
+var template = document.querySelector('#picture-template').content;
+var usersPhotos = [
+  {
+    url: '../kekstagram/photos/'+i+'.jpg',
+    comment: COMMENTS_PHRASES[commentsPhotosRandIndex],
+    like: randomInt(15, 200)
+  },
+    {
+    url: '../kekstagram/photos/'+i+'.jpg',
+    comment: COMMENTS_PHRASES[commentsPhotosRandIndex],
+    like: randomInt(15, 200)
+  },
+  {
+    url: '../kekstagram/photos/'+i+'.jpg',
+    comment: COMMENTS_PHRASES[commentsPhotosRandIndex],
+    like: randomInt(15, 200)
+  },
+  {
+    url: '../kekstagram/photos/'+i+'.jpg',
+    comment: COMMENTS_PHRASES[commentsPhotosRandIndex],
+    like: randomInt(15, 200)
+  },
+  {
+    url: '../kekstagram/photos/'+i+'.jpg',
+    comment: COMMENTS_PHRASES[commentsPhotosRandIndex],
+    like: randomInt(15, 200)
+  },
+  {
+    url: '../kekstagram/photos/'+i+'.jpg',
+    comment: COMMENTS_PHRASES[commentsPhotosRandIndex],
+    like: randomInt(15, 200)
+  }
+];
+
+var renderUserPhoto = function (usersPhotos) {
+  var photoElement = template.cloneNode(true);
+  photoElement.querySelector('.picture > img').setAttribute('src','../kekstagram/photos/'+i+'.jpg');
+  photoElement.querySelector('.picture-comments').textContent = usersPhotos.comment;
+  photoElement.querySelector('.picture-likes').textContent = usersPhotos.like;
+  return photoElement;
 };
-var commentsPhotos = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var photoDataSet = [];
-for (i = 0; i <= 24; i++) {
-  var photoAddressesRand = Math.floor(Math.random() * photoAddresses.length);
-  var commentsPhotosRand = Math.floor(Math.random() * (commentsPhotos.length));
-  var numberLikesRand = randomInt(15, 200);
-  photoDataSet[i] = new randomDataSet(photoAddresses[photoAddressesRand],
-    numberLikes[numberLikesRand], commentsPhotos[commentsPhotosRand]);
-};
-console.log(photoDataSet);
-
-//функция создания DOM-элемента на основе JS-объекта
-function createDocumentPictures() {
-  var fragment = document.createDocumentFragment()
-  var pic = document.createElement('img')
-  var likes = document.createElement('span')
-  var comment = document.createElement('span')
-
-
-  fragment.appendChild(pic)
-  pic.innerHTML = "<img src='"+photoAddresses[photoAddressesRand]+"'>";
-  fragment.appendChild(likes)
-  likes.innerHTML = "<span class=\"picture-stat picture-likes\">"+numberLikes[numberLikesRand]+"</span>";
-  fragment.appendChild(comment)
-  comment.innerHTML = "<span class=\"picture-stat picture-comments\">"+commentsPhotos[commentsPhotosRand]+"</span>";
-
-  return fragment
+var linksPictureTemplate = document.querySelector('.picture');
+var fragment = document.createDocumentFragment();
+for (var i = 1; i < usersPhotos.length; i++) {
+  fragment.appendChild(renderUserPhoto(usersPhotos[i]));
 }
-console.log(createDocumentPictures());
+template.appendChild(fragment);
+console.log(template);
 
+var renderUserPhoto = function (usersPhotos) {
+  var photoElement = gallery.cloneNode(true);
+  photoElement.querySelector('.gallery-overlay-preview > img').setAttribute('src','../kekstagram/photos/'+i+'.jpg');
+  photoElement.querySelector('.comments-count').textContent = usersPhotos.comment;
+  photoElement.querySelector('.likes-count').textContent = usersPhotos.like;
+  return photoElement;
+};
+
+var galleryOverlayPreview = document.querySelector('.gallery-overlay-preview');
+var fragment = document.createDocumentFragment();
+for (var i = 1; i < usersPhotos.length; i++) {
+  fragment.appendChild(renderUserPhoto(usersPhotos[i]));
+}
+gallery.appendChild(fragment);
