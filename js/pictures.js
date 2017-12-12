160c22ce6d2ef8aedae94065a5e93b5d5ec7a6af
Anya Vigolajnen <avi26.job@yandex.ru>
2017-12-13 02:50:52 +0300

    правки по выводы блоков pictures и gallery

--- js/pictures.js
+++ js/pictures.js
@@ -1,33 +1,57 @@
 'use strict';
 
 var COMMENTS_PHRASES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var commentsPhotosRandIndex = Math.floor(Math.random() * (COMMENTS_PHRASES.length));
+var gallery = document.querySelector('.gallery-overlay');
+gallery.classList.remove('hidden');
+var galleryPreview = gallery.querySelector('.gallery-overlay-preview');
+
+var pictures = document.querySelector('.pictures');
+var template = document.querySelector('#picture-template');
+var pictureTemplate = template.content.querySelector('.picture');
 
 function randomInt(min, max) {
   var rand = min - 0.5 + Math.random() * (max - min + 1);
   rand = Math.round(rand);
   return rand;
 }
-var gallery = document.querySelector('.gallery-overlay');
-gallery.classList.remove('hidden');
 
-var renderUserPhoto = function (usersPhotos) {
-  var photoElement = gallery.cloneNode(true);
-  photoElement.querySelector('.gallery-overlay-preview > img').setAttribute('src', usersPhotos.url);
-  photoElement.querySelector('.comments-count').textContent = usersPhotos.comment;
-  photoElement.querySelector('.likes-count').textContent = usersPhotos.like;
+var usersPhotos = [];
+usersPhotos.length = 25;
+for (var j = 0; j < 25; j++) {
+  COMMENTS_PHRASES.length = randomInt(1, 2);
+  usersPhotos[j] = {
+    url: '../kekstagram/photos/' + (j + 1) + '.jpg',
+    likes: randomInt(15, 200),
+    comments: COMMENTS_PHRASES
+  };
+}
+
+var renderUserPhoto = function (usersPhoto) {
+  var photoElement = pictureTemplate.cloneNode(true);
+
+  photoElement.setAttribute('href', usersPhoto.url);
+  photoElement.querySelector('img').setAttribute('src', usersPhoto.url);
+  photoElement.querySelector('.picture-comments').textContent = usersPhoto['comments'].length = randomInt(1, 2);
+  photoElement.querySelector('.picture-likes').textContent = usersPhoto.likes;
+
+  return photoElement;
+};
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < usersPhotos.length; i++) {
+  fragment.appendChild(renderUserPhoto(usersPhotos[i]));
+}
+pictures.appendChild(fragment);
+
+var renderUserPhotoGallery = function (usersPhotoGallery) {
+  var photoElement = galleryPreview.cloneNode(true);
+
+  photoElement.querySelector('img').setAttribute('src', usersPhotoGallery.url);
+  photoElement.querySelector('.comments-count').textContent = usersPhotoGallery.comments;
+  photoElement.querySelector('.likes-count').textContent = usersPhotoGallery.likes;
+
   return photoElement;
 };
 
-var fragment = document.createDocumentFragment();
-var usersPhotos = [];
-usersPhotos.length = 25;
-for (var j = 1; j < usersPhotos.length; j++) {
-  usersPhotos[j] = {
-    url: '../kekstagram/photos/' + j + '.jpg',
-    comment: COMMENTS_PHRASES[commentsPhotosRandIndex],
-    like: randomInt(15, 200)
-  };
-  fragment.appendChild(renderUserPhoto(usersPhotos[j]));
-}
-gallery.appendChild(fragment);
+var fragmentGallery = document.createDocumentFragment();
+fragmentGallery.appendChild(renderUserPhotoGallery(usersPhotos[0]));
+gallery.replaceChild(fragmentGallery, gallery.childNodes[3]);
