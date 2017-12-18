a91edf4a46c193df65f188fe57396d061488d38f
Anna Vigolajnen <vigolajnen@gmail.com>
2017-12-18 17:49:22 +0300

    добавила функцию вывода случайных комментариев

--- js/pictures.js
+++ js/pictures.js
@@ -1,8 +1,13 @@
 'use strict';
 
 var COMMENTS_PHRASES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var COUNT_PHOTOS = 25;
 var gallery = document.querySelector('.gallery-overlay');
+gallery.classList.remove('hidden');
+var galleryPreview = gallery.querySelector('.gallery-overlay-preview');
+
+var pictures = document.querySelector('.pictures');
+var template = document.querySelector('#picture-template');
+var pictureTemplate = template.content.querySelector('.picture');
 
 function randomInt(min, max) {
   var rand = min - 0.5 + Math.random() * (max - min + 1);
@@ -10,7 +15,6 @@ function randomInt(min, max) {
   return rand;
 }
 
-// генерация случайных комментариев
 var createRandomComments = function (comment) {
   var commentsArray = [];
   var countComment = randomInt(1, 2);
@@ -24,53 +28,44 @@ var createRandomComments = function (comment) {
   return commentsArray;
 };
 
-// генерация фотографий
-var createUserPhotos = function (count) {
-  var usersPhotos = [];
-  for (var j = 0; j < count; j++) {
-    usersPhotos[j] = {
-      url: '../kekstagram/photos/' + (j + 1) + '.jpg',
-      likes: randomInt(15, 200),
-      comments: createRandomComments(COMMENTS_PHRASES)
-    };
-  }
-  return usersPhotos;
-};
+var usersPhotos = [];
+usersPhotos.length = 25;
+for (var j = 0; j < 25; j++) {
+  COMMENTS_PHRASES.length = randomInt(1, 2);
+  usersPhotos[j] = {
+    url: '../kekstagram/photos/' + (j + 1) + '.jpg',
+    likes: randomInt(15, 200),
+    comments: createRandomComments(COMMENTS_PHRASES)
+  };
+}
 
-// создания DOM - элемента
 var renderUserPhoto = function (usersPhoto) {
-  var template = document.querySelector('#picture-template');
-  var pictureTemplate = template.content.querySelector('.picture');
   var photoElement = pictureTemplate.cloneNode(true);
 
+  photoElement.setAttribute('href', usersPhoto.url);
   photoElement.querySelector('img').setAttribute('src', usersPhoto.url);
   photoElement.querySelector('.picture-comments').textContent = usersPhoto['comments'].length;
   photoElement.querySelector('.picture-likes').textContent = usersPhoto.likes;
 
-  renderUserPhotoBig(usersPhoto);
-  gallery.classList.remove('hidden');
+  return photoElement;
+};
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < usersPhotos.length; i++) {
+  fragment.appendChild(renderUserPhoto(usersPhotos[i]));
+}
+pictures.appendChild(fragment);
+
+var renderUserPhotoGallery = function (usersPhotoGallery) {
+  var photoElement = galleryPreview.cloneNode(true);
+
+  photoElement.querySelector('img').setAttribute('src', usersPhotoGallery.url);
+  photoElement.querySelector('.comments-count').textContent = usersPhotoGallery['comments'].length;
+  photoElement.querySelector('.likes-count').textContent = usersPhotoGallery.likes;
 
   return photoElement;
 };
 
-// заполнение блока на основе созданных DOM - элементов
-var fillUserPhoto = function (photos) {
-  var pictures = document.querySelector('.pictures');
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < photos.length; i++) {
-    fragment.appendChild(renderUserPhoto(photos[i]));
-  }
-  pictures.appendChild(fragment);
-};
-
-var renderUserPhotoBig = function (photos) {
-  gallery.querySelector('img').setAttribute('src', photos.url);
-  gallery.querySelector('.comments-count').textContent = photos['comments'].length;
-  gallery.querySelector('.likes-count').textContent = photos.likes;
-};
-
-var galleryRender = function () {
-  fillUserPhoto(createUserPhotos(COUNT_PHOTOS));
-};
-galleryRender();
+var fragmentGallery = document.createDocumentFragment();
+fragmentGallery.appendChild(renderUserPhotoGallery(usersPhotos[0]));
+gallery.replaceChild(fragmentGallery, gallery.childNodes[3]);
 
