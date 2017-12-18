ac315196dc38b979b6829ba245afb66c65bff275
Anna Vigolajnen <vigolajnen@gmail.com>
2017-12-18 16:12:58 +0300

    изменила общий вывод фотографий

--- js/pictures.js
+++ js/pictures.js
@@ -1,33 +1,76 @@
 'use strict';
 
 var COMMENTS_PHRASES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var commentsPhotosRandIndex = Math.floor(Math.random() * (COMMENTS_PHRASES.length));
+var COUNT_PHOTOS = 25;
+var gallery = document.querySelector('.gallery-overlay');
 
 function randomInt(min, max) {
   var rand = min - 0.5 + Math.random() * (max - min + 1);
   rand = Math.round(rand);
   return rand;
 }
-var gallery = document.querySelector('.gallery-overlay');
-gallery.classList.remove('hidden');
 
-var renderUserPhoto = function (usersPhotos) {
-  var photoElement = gallery.cloneNode(true);
-  photoElement.querySelector('.gallery-overlay-preview > img').setAttribute('src', usersPhotos.url);
-  photoElement.querySelector('.comments-count').textContent = usersPhotos.comment;
-  photoElement.querySelector('.likes-count').textContent = usersPhotos.like;
+// генерация случайных комментариев
+var createRandomComments = function (comment) {
+  var commentsArray = [];
+  var countComment = randomInt(1, 2);
+  if (countComment === 1) {
+    commentsArray.push(comment[randomInt(0, comment.length - 1)]);
+  } else {
+    for (var k = 1; k <= 2; k++) {
+      commentsArray.push(comment[randomInt(0, comment.length - 1)]);
+    }
+  }
+  return commentsArray;
+};
+
+// генерация фотографий
+var createUserPhotos = function (count) {
+  var usersPhotos = [];
+  for (var j = 0; j < count; j++) {
+    usersPhotos[j] = {
+      url: '../kekstagram/photos/' + (j + 1) + '.jpg',
+      likes: randomInt(15, 200),
+      comments: createRandomComments(COMMENTS_PHRASES)
+    };
+  }
+  return usersPhotos;
+};
+
+// создания DOM - элемента
+var renderUserPhoto = function (usersPhoto) {
+  var template = document.querySelector('#picture-template');
+  var pictureTemplate = template.content.querySelector('.picture');
+  var photoElement = pictureTemplate.cloneNode(true);
+
+  photoElement.querySelector('img').setAttribute('src', usersPhoto.url);
+  photoElement.querySelector('.picture-comments').textContent = usersPhoto['comments'].length;
+  photoElement.querySelector('.picture-likes').textContent = usersPhoto.likes;
+
+  renderUserPhotoBig(usersPhoto);
+  gallery.classList.remove('hidden');
+
   return photoElement;
 };
 
-var fragment = document.createDocumentFragment();
-var usersPhotos = [];
-usersPhotos.length = 25;
-for (var j = 1; j < usersPhotos.length; j++) {
-  usersPhotos[j] = {
-    url: '../kekstagram/photos/' + j + '.jpg',
-    comment: COMMENTS_PHRASES[commentsPhotosRandIndex],
-    like: randomInt(15, 200)
-  };
-  fragment.appendChild(renderUserPhoto(usersPhotos[j]));
-}
-gallery.appendChild(fragment);
+// заполнение блока на основе созданных DOM - элементов
+var fillUserPhoto = function (photos) {
+  var pictures = document.querySelector('.pictures');
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < photos.length; i++) {
+    fragment.appendChild(renderUserPhoto(photos[i]));
+  }
+  pictures.appendChild(fragment);
+};
+
+var renderUserPhotoBig = function (photos) {
+  gallery.querySelector('img').setAttribute('src', photos.url);
+  gallery.querySelector('.comments-count').textContent = photos['comments'].length;
+  gallery.querySelector('.likes-count').textContent = photos.likes;
+};
+
+var galleryRender = function () {
+  fillUserPhoto(createUserPhotos(COUNT_PHOTOS));
+};
+galleryRender();
+
